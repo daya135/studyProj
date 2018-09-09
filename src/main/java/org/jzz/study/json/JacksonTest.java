ea8e2134ac1770d1a3279704cfc68cbf4cbeb383
daya135 <Jzzperson@163.com>
2018-09-10 00:07:50 +0800

    aaa

--- src/main/java/org/jzz/study/json/JacksonTest.java
+++ src/main/java/org/jzz/study/json/JacksonTest.java
@@ -1,13 +1,20 @@
 package org.jzz.study.json;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationConfig;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class JacksonTest {
 	
@@ -45,6 +52,28 @@ public class JacksonTest {
 	    
 	}
 	
+	
+	public static void toJsonTreeMode() throws IOException {
+		JsonNodeFactory factory = new JsonNodeFactory(true);
+		JsonFactory jsonFactory = new JsonFactory();
+		JsonGenerator generator = jsonFactory.createGenerator(System.out);
+		ObjectMapper mapper = new ObjectMapper();
+		 
+        //创建节点和数据,一个ObjectNode代表一个节点对象
+        ObjectNode node1 = factory.objectNode();
+        ObjectNode node2 = factory.objectNode();
+        node1.put("A", "a");
+        node1.put("B", "b");
+        node2.set("C", node1);
+ 
+        // 根节点
+        ObjectNode root = factory.objectNode();
+        root.put("root", "root");
+        root.set("children", node2);
+        mapper.writeTree(generator, root);
+        //输出{"root":"root","children":{"C":{"A":"a","B":"b"}}}
+	}
+	
 	public static void toBeanTest() throws Exception{
 		ObjectMapper mapper = new ObjectMapper();
 		
@@ -58,8 +87,41 @@ public class JacksonTest {
 		System.out.println(beans.get(1));
 	}
 	
+	 public static void testTreeModel() throws Exception {
+	        ObjectMapper objectMapper = new ObjectMapper();
+			JsonNodeFactory nodeFactory = objectMapper.getNodeFactory();
+	        // 创建一个model
+	        ObjectNode node = nodeFactory.objectNode();
+	        node.put("age", 18);
+	        // 新增
+	        node.put("name", "周杰伦");
+	        // 如果存在同名的则是替换操作
+	        node.put("age", 19);
+	        ArrayNode coursesNode = node.putArray("courses");
+	        coursesNode.add("思想政治");
+	        coursesNode.add("高等数学");
+	        // 获取节点类型
+	        System.out.println(node.getNodeType());
+	        System.out.println(coursesNode.getNodeType());
+	        // 移除第一个
+	        coursesNode.remove(0);
+	        // 输出
+	        System.out.println(node.toString());
+	        String jsonStr = "{\"age\":19,\"name\":\"周杰伦\",\"courses\":[\"高等数学\"]}";
+	        JsonNode jsonNode = objectMapper.readTree(jsonStr);
+	        ArrayNode arrayNode = (ArrayNode) jsonNode.withArray("courses");
+	        arrayNode.add("马列");
+	        for (int i = 0;i < arrayNode.size();i++){
+	            System.out.println(arrayNode.get(i).asText());
+	        }
+	        System.out.println(jsonNode.toString());
+
+	    }
+	
 	public static void main(String[] args) throws Exception{
-		toJsonTest();
-		toBeanTest();
+//		toJsonTest();
+//		toBeanTest();
+		toJsonTreeMode();
+		testTreeModel();
 	}
 }
