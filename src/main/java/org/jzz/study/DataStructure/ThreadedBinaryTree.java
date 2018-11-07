539c221418100f9324af34ca14bf1d3d2c52413d
daya135 <Jzzperson@163.com>
2018-11-07 17:33:18 +0800

    tree

--- src/main/java/org/jzz/study/DataStructure/ThreadedBinaryTree.java
+++ src/main/java/org/jzz/study/DataStructure/ThreadedBinaryTree.java
@@ -2,13 +2,15 @@ package org.jzz.study.DataStructure;
 
 import java.util.LinkedList;
 
+import org.jzz.study.DataStructure.Tree.Node;
+
 
 /**
  * 线索二叉树
  */
 public class ThreadedBinaryTree<T> {
 	public int length = 0;
-	public Node<T> head = null;
+	public Node<T> root = null;
 	
 	static class  Node<T> {
 		public Node<T> left = null;
@@ -36,7 +38,7 @@ public class ThreadedBinaryTree<T> {
 			nodeList.add(node);
 		}
 		
-		head = nodeList.getFirst();
+		root = nodeList.getFirst();
 		this.length ++;
 		
 		for (int i = 0; i < arr.length / 2; i++) {
@@ -86,11 +88,11 @@ public class ThreadedBinaryTree<T> {
      * 中序线索化
      * 一旦完成线索化则不能再用常规方法遍历二叉树了，因为叶子节点的左右指针不为空了
      * @return first 头节点
-     * @param head 根节点
+     * @param root 根节点
      * */
-    public Node<T> inOrder(Node<T> head) {
+    public Node<T> inOrder(Node<T> root) {
     	Node<T> first = new Node<T>();	//头节点，注意不是根节点！！！
-    	Node<T> p = head;
+    	Node<T> p = root;
     	Node<T> pre = null;
     	int firstFlag = 0;
     	LinkedList<Node<T>> listStack = new LinkedList<Node<T>>();
@@ -124,19 +126,19 @@ public class ThreadedBinaryTree<T> {
 				p = p.right;
 			}
     	}
-    	first.left = head;
+    	first.left = root;
     	pre.right = first;
     	return first;
     }
     
-    public void inOrderTraverse(Node<T> first) {
-    	Node<T> p = first.left;	//头节点的左子树指向根节点
-    	while (p != first) {
+    public void inOrderTraverse(Node<T> head) {
+    	Node<T> p = head.left;	//头节点的左子树指向根节点
+    	while (p != head) {
     		while (p.lTag == 0) {
     			p = p.left;
     		}
     		System.out.print(p.data + " ");
-    		while (p.rTag == 1 && p.right != first) {
+    		while (p.rTag == 1 && p.right != head) {
     			if (p.right != null) {
     				p = p.right;
     				System.out.print(p.data + " ");
@@ -148,19 +150,68 @@ public class ThreadedBinaryTree<T> {
     	}
     }
     
+    public Node<T> testBuild() {
+    	LinkedList<Node<T>> stack = new LinkedList<Node<T>>();
+    	Node<T> p = this.root;
+    	Node<T> pre = null;
+    	Node<T> head = new Node<T>();
+    	head.right = root;
+    	while(p != null || !stack.isEmpty()) {
+    		while (p!= null) {
+    			System.out.print(p.data + " ");
+    			if (p.left == null) {
+    				p.lTag = 1;
+    				if (pre != null ) {
+    					p.left = pre;
+    					if (pre.lTag == 1) {
+    						pre.right = p;
+    					}
+    				} 
+    			}
+    			if (p.right == null) {
+    				p.rTag = 1;
+    			}
+    			pre = p;
+    			if (p.lTag == 0) {
+    				stack.push(p);
+    				p = p.left;
+    			} else {
+    				p = null;
+    			}
+    		}
+    		
+    		if (!stack.isEmpty()) {
+    			p = stack.removeFirst();
+    			p = p.right;
+    		}
+    	}
+    	pre.right = head;
+    	head.left = pre;
+    	return head;
+    }
+    
+    public void testTraverse() {
+    	
+    }
+    
     public static void test() {
     	Integer a[] = {10, 8, 9, 4, 12, 6, 14, 3, 5, 11, 15, 7, 1, 13, 2};
 //		Integer a[] = {4, 2, 5, 1, 3, 7, 6};
     	ThreadedBinaryTree<Integer> tree = new ThreadedBinaryTree<Integer>();
 		tree.InitTree(a);
 		System.out.println("preOrder");
-		tree.preOrder(tree.head);
+		tree.preOrder(tree.root);
 //		System.out.println("\nInOrder_recursion");
-//		tree.InOrder_recursion(tree.head);
-		System.out.println("\ninOrder");
-		Node<Integer> first = tree.inOrder(tree.head);
-		System.out.println("\nfirst: " + first.data);
-		tree.inOrderTraverse(first);
+//		tree.InOrder_recursion(tree.root);
+//		System.out.println("\ninOrder");
+//		Node<Integer> head = tree.inOrder(tree.root);
+//		System.out.println("\nfirst: " + head.data);
+//		tree.inOrderTraverse(head);
+		System.out.println("\ntestbuild:");
+		tree.testBuild();
+		System.out.println("\ntestTraverse:");
+		tree.testTraverse();
+		
 	}
     
     public static void main(String[] args) {
--- src/main/java/org/jzz/study/DataStructure/Tree.java
+++ src/main/java/org/jzz/study/DataStructure/Tree.java
@@ -7,7 +7,7 @@ import org.jzz.study.util.Print;
 public class Tree<T> {
 	
 	public int length = 0;
-	public Node head = null;
+	public Node root = null;
 	
     class Node {
 		public Node left = null;
@@ -31,7 +31,7 @@ public class Tree<T> {
 			nodeList.add(node);
 		}
 		
-		head = nodeList.getFirst();
+		root = nodeList.getFirst();
 		this.length ++;
 		
 		for (int i = 0; i < arr.length / 2; i++) {
@@ -72,10 +72,10 @@ public class Tree<T> {
 			InOrder_recursion(p.right);
 		}
 	}
-	
-	public void PreOrder(Node head) {
+	/** 前序遍历,非递归算法1 自己的烂代码*/
+	public void PreOrder(Node root) {
 		LinkedList<Node> listStack = new LinkedList<Node>();
-		Node p = head;
+		Node p = root;
 		while (p != null) {
 			System.out.print(p.data + " ");
 			if (p.right != null) {
@@ -92,31 +92,48 @@ public class Tree<T> {
 			}
 		}
 	}
-	
-	public void InOrder(Node head) {
+	/** 前序遍历,非递归算法2 网上的优美代码 */
+	public void PreOrder2(Node root) {
 		LinkedList<Node> listStack = new LinkedList<Node>();
-		Node p = head;
+		Node p = root;
 		while (p != null || !listStack.isEmpty()) {
 			if (p != null) {
+				System.out.print(p.data + " ");
 				listStack.push(p);
 				p = p.left;
 			} else {
+				p = listStack.removeFirst();
+				p = p.right;
+			}
+		}
+	}
+	
+	/** 中序遍历，非递归 */
+	public void InOrder(Node root) {
+		LinkedList<Node> listStack = new LinkedList<Node>();
+		Node p = root;
+		while (p != null || !listStack.isEmpty()) {
+			while (p != null) {
+				listStack.push(p);
+				p = p.left;
+			}
+			if(!listStack.isEmpty()){
 				p = listStack.removeFirst();
 				System.out.print(p.data + " ");
 				p = p.right;
 			}
 		}
 	}
-	
-	public void PostOrder(Node head) {
+	/** 后序遍历，非递归，访问标志法 */
+	public void PostOrder(Node root) {
 		LinkedList<Node> listStack = new LinkedList<Node>();
-		Node p = head;
+		Node p = root;
 		Node last = p;
 		listStack.push(p);
 		while (!listStack.isEmpty()) {
 			p = listStack.getFirst();
-			if ((p.left == null && p.right == null)
-				|| (p.right == null && last == p.left) || (last == p.right)) {
+			if ((p.left == null && p.right == null) ||
+				(last != null) && (p.right == last && p.left == last)) {  //入栈顺序决定了不可能出现right不为空且last==left的情况，
 				System.out.print(p.data + " ");
 				last = p;
 				listStack.removeFirst();
@@ -131,10 +148,10 @@ public class Tree<T> {
 		}
 	}
 	
-
-	public void PostOrder2(Node head) {
+	/** 后序遍历，非递归，双栈法 */
+	public void PostOrder2(Node root) {
 		LinkedList<Node> listStack = new LinkedList<Node>();
-		Node p = head;
+		Node p = root;
 		listStack.push(p);
 		listStack.push(p);
 		while (!listStack.isEmpty()) {
@@ -152,7 +169,43 @@ public class Tree<T> {
 				System.out.print(p.data + " ");
 			}
 		}
-		
+	}
+	
+	public void layerOrder() {
+		LinkedList<Node> listStack = new LinkedList<Node>();
+		Node p = this.root;
+		listStack.push(p);
+		while(!listStack.isEmpty()) {
+			p = listStack.removeLast();
+			System.out.print(p.data + " ");
+			if (p.left != null) {
+				listStack.push(p.left);
+			}
+			if (p.right != null) {
+				listStack.push(p.right);
+			}
+		}
+	}
+	
+	public void testOrder() {
+		LinkedList<Node> listStack = new LinkedList<Node>();
+		Node p = this.root;
+		Node pre = null;
+		listStack.push(p);
+		while(!listStack.isEmpty()) {
+			p = listStack.getFirst();
+			if ((p.left == null && p.right == null) ||
+					(pre != null &&(p.left == pre || p.right == pre))) {
+				System.out.print(p.data + " ");
+				pre = p;
+				listStack.removeFirst();
+			} else {
+				if (p.right != null)
+					listStack.push(p.right);
+				if (p.left != null) 
+					listStack.push(p.left);
+			}
+		}
 	}
 	
 	
@@ -161,19 +214,25 @@ public class Tree<T> {
 //		Integer a[] = {4, 2, 5, 1, 3, 7, 6};
 		Tree<Integer> tree = new Tree<Integer>();
 		tree.InitTree(a);
-		tree.PreOrder_recursion(tree.head);
+		System.out.println("layerOrder");
+		tree.layerOrder();
+		System.out.println("\nPreOrder_recursion");
+		tree.PreOrder_recursion(tree.root);
+		System.out.println("\nPostOrder_recursion");
+		tree.PostOrder_recursion(tree.root);
+		System.out.println("\nInOrder_recursion");
+		tree.InOrder_recursion(tree.root);
+		System.out.println("\nPreOrder");
+		tree.PreOrder(tree.root);
 		System.out.println();
-		tree.PostOrder_recursion(tree.head);
-		System.out.println();
-		tree.InOrder_recursion(tree.head);
-		System.out.println();
-		tree.PreOrder(tree.head);
-		System.out.println();
-//		tree.PostOrder(tree.head);
-		tree.PostOrder2(tree.head);
-		System.out.println();
-		tree.InOrder(tree.head);
-		
+		tree.PreOrder2(tree.root);
+		System.out.println("\nPostOrder");
+//		tree.PostOrder(tree.root);
+		tree.PostOrder2(tree.root);
+		System.out.println("\nInOrder");
+		tree.InOrder(tree.root);
+		System.out.println("\ntestOrder");
+		tree.testOrder();
 		
 	}
 	
